499344d4baa5ad549dc5075e13ecec68ad4a4328
nikhithabandi <nikhithabandi@gmail.com>
2026-02-13 15:16:30 +0530

    message

--- IV th Sem/python/Assignments/AST01/task.py
+++ IV th Sem/python/Assignments/AST01/task.py
@@ -1,7 +1,11 @@
 def Ticket_Pricing(n: int) -> int:
-   pass
-
-
+    if n<10:
+        return 10
+    elif n %10 == 0:
+        return 15
+    else:
+        return 20
 if __name__ == '__main__':
-    n = int(input())
+    n = int(input("Enter number of tickets:"))
     print(Ticket_Pricing(n))
+
---
+++ IV th Sem/python/Practice/MO2_Logic_Building_&_Patterns/S03/PS03_series.py
@@ -0,0 +1,22 @@
+#1.fibonocci series:
+#n=int(input("enter a num: "))
+#a=0
+#b=1
+#print("fibonocci series: ")
+#for i in range(n):
+#    print(a,end=' ')
+#    c=a+b
+#    a=b
+#   b=c
+#(or)
+#li=[0,1]
+#for i in range(2,n):
+# li.append(li[i-2]+li[i-1])
+#print(li)
+
+#2.arthmetic series:
+
+"'power of a number'"
+n=int(input())
+for i in range(1,11):
+    print(n**i,end=" ")
