aeeeb5d8a87742d6e6c3e5dfa20be6cbb153a601
nikhithabandi <nikhithabandi@gmail.com>
2026-02-16 14:54:06 +0530

    msg

---
+++ IV th Sem/python/Practice/MO2_Logic_Building_&_Patterns/S04/PS05_Basic_patterns.py
@@ -0,0 +1,92 @@
+'''
+input:4
+****
+****
+****
+****
+'''
+#n=int(input())
+#for i in range(n):
+#    for j in range(n):
+#        print("*",end=' ')
+#    print()
+
+'''n=4
+*
+**
+***
+****
+'''
+#n=int(input())
+#for i in range(1,n+1):
+#    for j in range(1,i+1):
+#        print("*",end=' ')
+#    print()
+
+'''n=4
+****
+***
+**
+*
+'''
+#n=int(input())
+#for i in range(n,0,-1): ->n
+#    for j in range(i): ->n-i,0,-1
+#        print("*",end=' ')
+#    print()
+
+'''
+n=4
+1
+23
+456
+78910
+'''
+#n=int(input())
+#num=1
+#for i in range(1,n+1):
+#    for j in range(1,i+1):
+#        print(num,end=' ')
+#        num+=1
+#    print()
+
+'''
+print(ord('A'))
+print(char(65))
+n=4
+A
+AB
+ABC
+ABCD
+'''
+#n=int(input())
+#for i in range(n):
+#    for j in range(i+1):
+#        print(chr(65+j),end=' ')
+#    print()
+
+'''
+n=4
+A
+BC
+DEF
+GHIJ
+KLMNO
+'''
+#n=int(input())
+#k=65
+#for i in range(n+1):
+#     for j in range(i+1):
+#        print(chr(k),end=' ')
+#        k+=1
+#    print()
+
+'''
+n=4
+****
+*  *
+*  *
+****
+'''
+n=int(input())
+for i in range()
